823a2fe1224f338af2ab534312cc0c890ef0ff79
H.G.J.U. Gunathunga <126705344+janithush@users.noreply.github.com>
2025-04-14 03:52:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+//create web server
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Comment = require("./models/comment");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/comments", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Get all comments
+app.get("/comments", async (req, res) => {
+  try {
+    const comments = await Comment.find();
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
